5f9eb501cad6937379985d8e1498586265e4b94c
David-T7 <tesfayedawit524@gmail.com>
2023-11-23 03:05:24 -0800

    static files created

--- instructor_evaluation/urls.py
+++ instructor_evaluation/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     path('',include('Staff.urls')),
     path('admin/', admin.site.urls),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
